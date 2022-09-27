49ad55e2ae81e97579ebaa7788fea2740e5183d3
Yotam Granada <yotam.granada@gmail.com>
2022-09-27 21:39:13 +0300

    ex #2 completed

--- exercises/exercise_2.js
+++ exercises/exercise_2.js
@@ -2,6 +2,7 @@ const characters = require("../data/characters.json");
 
 // TODO: return all characters with known birth year
 const charactersWithKnownBirthDate = () => {
+  return characters.filter(character => character.birth_year !== "unknown")
   // CODE HERE
 };
 
--- exercises/exercise_3.js
+++ exercises/exercise_3.js
@@ -6,8 +6,15 @@ const characters = require("../data/characters.json");
 const charactersWithMediumHeight = () => {
   // YOUR CODE HERE
   // YOU CAN CHANGE ANY CODE INSIDE THE FUNCTION
-  const MIN_HEIGHT = 0;
-  const MAX_HEIGHT = 300;
+  const MIN_HEIGHT = 180;
+  const MAX_HEIGHT = 200;
+
+  return characters.filter((character) => {
+    console.log(character.height);
+    return character.height >= MIN_HEIGHT && character.height <= MAX_HEIGHT;
+  });
 };
+
+charactersWithMediumHeight()
 
 module.exports = charactersWithMediumHeight;
